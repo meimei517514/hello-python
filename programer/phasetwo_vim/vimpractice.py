4169695979d8f14f4bf632fa5f5951b25fd3e011
maliying <406550759@qq.com>
2016-07-08 01:56:38 -0700

    daily practice

---
+++ programer/phasetwo_vim/vimpractice.py
@@ -0,0 +1,71 @@
+sdfaasdfsdfsdjkdsfasdflfasdf
+sdfaasdfsdfsdfasdf sdfaasdfsdfsdfasdf JDDsdfaasdfidsdfdsdfsdfasdf jdsaflk sdfaasdfisdfsdfasdf djflskdfjl JDDsdfaasdfisdx djflskdfjl
+O
+JKJKJKJKJ
+jjjjjjjjjjjjjjjjjjjjjjjjj jjjjjjjjjjjjjj dafsdfasdfda sdfdfsdfasdf 
+JKSDJFLASKDJ
+JJ
+JJ
+
+JJ
+
+sdfaasdfsdfsdfasdf
+jjjjjjjjjjjjjjjjjjjjjjjjj jjjjjjjjjjjjjj dafsdfasdfda sdfdfsdfasdf 
+jjjjjjjjjjjjjjjjjjjjjjjjj jjjjjjjjjjjjjj dafsdfasdfda sdfdfsdfasdf 
+JKJKJKJKJ
+
+
+desudesudesudesudesudesudesudesudesudesuO
+jjj
+JKJKJKJKJ
+JKJKJKJKJ
+iikjkljlJKJKJKJKJ
+desudesudesudesudesudesudesudesudesudesdesudesudesudesudesudesudesudesudesudesuu
+
+JK
+
+JJKJKJKJKJKJKJKJKJ
+
+JJKJJKJdKJKJKJdKJ JKKJdKJKJKJdKJ JKJKJKJJKJJKJJ  JJJSDAFA sdfaasdfsdfsdfasdf sdfaasdfisdfsdfasdf djflskdfjl o jdskfjkjkljkjkk O JDDsdfaasdfisdfsdfasdf djflskdfjl sdfaasdfisdfsdfasdf Djflskd
+    ,,,    *JKJKJKJ JKJKJKJKJ JJJSDAFA sdfaasdfsdfsdfasdf sdfaasdfisdfsdfasdf djflskdfjl o jdskfjkjkljkjkk O JDDsdfaasdfisdfsdfasdf djflskdfjl sdfaasdfisdfsdfasdf,djflskd             sdfas            ,,
+{JKJKJKJ {JKJKJKJKJ JJJSDAFA {}}JSDFAASDFSDFSDFASDF SDFAASDFISDFSDFASD[]F DJFLSKDFJL []O JDSKFJKJKLJKJKK O JDDSDFAASDFISDFSDFASDF DJFLSKDFJ()L SDFAASDFISDFSDFASDF DJFLSKD
+()
+
+dsfskaljj
+desudesudesuadfsdafdflkjaadflksdjfsladk"
+jjjjjjjjjjjjjjjjjjjffjl
+D--jjjjjjjjjjjjjjjjjjjffjla D--jjjjjjjjjjjjjjjjjjDAFDAFAAJFFJLF
+jjjjjjjjjjjjjjjjjjjjjjjj
+dlfksdjj j
+D--10d10dijpnjjjjjjjj10dJJJJJJJJJJJJJJJJJJJFFJLE
+D--JJJJJJJJJJJJJJJJJJJFFJLG
+D--Jjjjjjffjl
+D
+(map (+) ("foo"))
+jdskfjkjkljkjkk
+g
+jjjjjj
+2 3ABC 1 2  ABC
+kkLJ4 5ABC 6
+kkijLJ7  ABiiC
+LJ8  ABC
+		  LJ9  ABC
+LJ10 ABC
+LJ11 ABC
+LJ12 ABC
+LJ13 ABC
+LJ14 ABC"
+
+import os
+
+def functest():
+	print a
+	print b
+
+	print c
+
+		for i in range(9):
+			print i
+
+
+
